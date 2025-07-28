f580b17819889bc044c9f78b261e34a23db6983b
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2025-07-28 18:06:20 +0000

    Release 0.0.7

---
+++ src/errors/index.ts
@@ -0,0 +1,2 @@
+export { DevinTestClientError } from "./DevinTestClientError.js";
+export { DevinTestClientTimeoutError } from "./DevinTestClientTimeoutError.js";
---
+++ src/index.ts
@@ -0,0 +1,3 @@
+export * as DevinTestClient from "./api/index.js";
+export { DevinTestClientError, DevinTestClientTimeoutError } from "./errors/index.js";
+export { DevinTestClientClient } from "./Client.js";
